185bad2bf5d7d91b479b595f1102f9dcbc918b15
Ankit <ankitbhrdwj27@gmail.com>
2014-01-19 14:14:36 +0530

    Update kvstore.go

--- kvstore.go
+++ kvstore.go
@@ -1,38 +1,32 @@
 package main
 
 import (
-	"net/http" 
-	"io"
 	"fmt"
-	"sync"
+	"io"
+	"net/http"
 	"strings"
-	)
+	"sync"
+)
 
 var mutex sync.Mutex
 var hash map[string]string
 
-
-
-
 func main() {
-mutex.Lock()
-hash=make(map[string]string)
-hash["first"]="demo"
-mutex.Unlock()
-http.HandleFunc("/index", index)
-go http.HandleFunc("/insert/", insert)
-go http.HandleFunc("/deletekv/", deletekv)
-go http.HandleFunc("/check/", check)
-http.ListenAndServe(":8080", nil)
+	mutex.Lock()
+	hash = make(map[string]string)
+	hash["first"] = "demo"
+	mutex.Unlock()
+	http.HandleFunc("/index", index)
+	go http.HandleFunc("/insert/", insert)
+	go http.HandleFunc("/deletekv/", deletekv)
+	go http.HandleFunc("/check/", check)
+	http.ListenAndServe(":8080", nil)
 }
 
-
-
-
 func index(res http.ResponseWriter, req *http.Request) {
-res.Header().Set("Content-Type","text/html")
-io.WriteString(res,
-`<doctype html>
+	res.Header().Set("Content-Type", "text/html")
+	io.WriteString(res,
+		`<doctype html>
 <html>
 <head>
 <title>kvstore</title>
@@ -47,54 +41,47 @@ io.WriteString(res,
 </body>
 <footer><sup>**</sup>Key and Value can be of string type only.And key-value store work properly for the above given commands.
 </html>`,
-)
+	)
 }
-
-
-
 
 func insert(res http.ResponseWriter, req *http.Request) {
-key:=strings.Split(req.URL.Path[8:],"=")
-var Key,Value string
-if key[0]!=""{
-Key=key[0]
+	key := strings.Split(req.URL.Path[8:], "=")
+	var Key, Value string
+	if key[0] != "" {
+		Key = key[0]
+	}
+	if key[1] != "" {
+		Value = key[1]
+	}
+	if Key != "" && Value != "" {
+		mutex.Lock()
+		hash[Key] = Value
+		fmt.Fprintf(res, "KEY = %s \t VALUE = %s", Key, Value)
+		mutex.Unlock()
+	} else {
+		fmt.Fprintf(res, "Give argumets properly in the form  KEY=VALUE")
+	}
 }
-if key[1]!=""{
-Value=key[1]
-}
-if Key!="" && Value!=""{
-mutex.Lock() 
-hash[Key]=Value
-fmt.Fprintf(res,"KEY = %s \t VALUE = %s",Key,Value)
-mutex.Unlock()
-} else{
-fmt.Fprintf(res,"Give argumets properly in the form  KEY=VALUE")
-}
-}
-
-
 
 func deletekv(res http.ResponseWriter, req *http.Request) {
-key:=strings.Split(req.URL.Path[10:],"\n")
-mutex.Lock()
-if key[0]!="" && hash[key[0]]!=""{
-fmt.Fprintf(res,"Pair deleted is KEY=%s \t VALUE=%s",key[0],hash[key[0]])
-delete(hash,key[0])
-}else{
-fmt.Fprintf(res,"Given KEY=VALUE pair is not available in KVSTORE")
+	key := strings.Split(req.URL.Path[10:], "\n")
+	mutex.Lock()
+	if key[0] != "" && hash[key[0]] != "" {
+		fmt.Fprintf(res, "Pair deleted is KEY=%s \t VALUE=%s", key[0], hash[key[0]])
+		delete(hash, key[0])
+	} else {
+		fmt.Fprintf(res, "Given KEY=VALUE pair is not available in KVSTORE")
+	}
+	mutex.Unlock()
 }
-mutex.Unlock()
-}
-
-
 
 func check(res http.ResponseWriter, req *http.Request) {
-key:=strings.Split(req.URL.Path[7:],"\n")
-mutex.Lock()
-if hash[key[0]]!=""{
-fmt.Fprintf(res,"KEY=%s \t VALUE=%s",key[0],hash[key[0]])
-}else{
-fmt.Fprintf(res,"Given KEY=VALUE pair is not available in KVSTORE")
-}
-mutex.Unlock()
+	key := strings.Split(req.URL.Path[7:], "\n")
+	mutex.Lock()
+	if hash[key[0]] != "" {
+		fmt.Fprintf(res, "KEY=%s \t VALUE=%s", key[0], hash[key[0]])
+	} else {
+		fmt.Fprintf(res, "Given KEY=VALUE pair is not available in KVSTORE")
+	}
+	mutex.Unlock()
 }
